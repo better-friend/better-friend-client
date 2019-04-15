9b979dd1a72a11b681a0cc31a9b9530132fd5a14
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-15 17:16:27 -0400

    Added routes and navlinks for navigating components

--- better-friends/src/App.js
+++ better-friends/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { Route, NavLink } from 'react-router-dom'
+import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Friends from './components/Friends';
@@ -9,10 +11,27 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Better Friends</h1>
-        <Login />
-        <Signup />
-        <Friends />
+        <ul className="navBar">
+          <li>
+            <NavLink exact to="/" className="activeNav">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/signup" className="activeNav">
+              Create Account
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" className="activeNav">
+              Login
+            </NavLink>
+          </li>
+        </ul>
+        <Route exact path='/' component={Home}/>
+        <Route path='/login' component={Login}/>
+        <Route path='/signup' component={Signup}/>
+        <Route path='/friends' component={Friends}/>
       </div>
     );
   }
