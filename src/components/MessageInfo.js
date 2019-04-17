ec595f7b6e855f46069adaab681e794a170be3db
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-16 20:33:27 -0400

    Removed map function for mapping over props

--- src/components/MessageInfo.js
+++ src/components/MessageInfo.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const MessageInfo = (props) => {
+const MessageInfo = props => {
 
     return (
         <div>
             <h2>Messages ready to be sent</h2>
-            {props.message.map((message, id) => (
-                <div key={id}>
-                    <p>Recipient: {message.person}</p>
-                    <p>Phone Number: {message.phone}</p>
-                    <p>Message: {message.message}</p>
-                    <p>Send Date: {message.date}</p>
+                <div>
+                    <p>Recipient: {props.message.person}</p>
+                    <p>Phone Number: {props.message.phone}</p>
+                    <p>Message: {props.message.message}</p>
+                    <p>Send Date: {props.message.date}</p>
                 </div>
-            ))}
         </div>
     )
 }
