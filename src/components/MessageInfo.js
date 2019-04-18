58eddd38026ae0d80f45aeb5fd8176494eb6abbe
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-18 00:15:27 -0400

    Commented out Card imports from reactstrap until later

--- src/components/MessageInfo.js
+++ src/components/MessageInfo.js
@@ -1,16 +1,19 @@
 import React from 'react';
+// import { Card, CardImg, CardText, CardBody,
+//     CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 const MessageInfo = props => {
 
     return (
         <div>
-            <h2>Messages ready to be sent</h2>
-                <div>
-                    <p>Recipient: {props.message.person}</p>
-                    <p>Phone Number: {props.message.phone}</p>
-                    <p>Message: {props.message.message}</p>
-                    <p>Send Date: {props.message.date}</p>
-                </div>
+            {/* <Card>
+                <CardBody> */}
+                <p>Recipient: {props.message.person}</p>
+                <p>Phone Number: {props.message.phone}</p>
+                <p>Message: {props.message.message}</p>
+                <p>Send Date: {props.message.date}</p>
+                {/* </CardBody>
+            </Card> */}
         </div>
     )
 }
