de7982b30707f47564d1b176a1724837586cb9c2
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-18 00:13:17 -0400

    Syntax changes

--- src/components/MessageContainer.js
+++ src/components/MessageContainer.js
@@ -4,6 +4,7 @@ import MessageInfo from './MessageInfo';
 const MessageContainer = props => {
     return(
         <div>
+            <h2>Messages ready to be sent</h2>
             {props.messageData.map((message, id) => {
                 return (
                     <div key={id}>
